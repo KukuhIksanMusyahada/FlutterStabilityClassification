a135bdeedfa1ee182538ac6e5ce47694c072e42a
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-07-08 22:43:41 +0700

    upload path_handling.py

---
+++ Script/path_handler.py
@@ -0,0 +1,17 @@
+import os
+
+def get_this_dir():
+    return os.path.dirname( os.path.abspath(__file__) )
+
+def get_data_source():
+    return os.path.join(get_this_dir(), os.pardir, 'Data_Source')
+
+def get_raw_data():
+    return os.path.join(get_data_source(), 'Raw')
+
+def get_processed_data():
+    return os.path.join(get_data_source(), 'Processed_Data')
+
+
+
+
