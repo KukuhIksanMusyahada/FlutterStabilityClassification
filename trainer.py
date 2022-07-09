564c1f4de8888f6891812dbe1fdc052ecca0d177
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-07-09 14:51:46 +0700

    resolved modelsave error (cant pick history file)

--- trainer.py
+++ trainer.py
@@ -5,18 +5,13 @@ from Script import data_processing as dp
 from Script import models
 
 
-def trainer():
+def run_trainer():
     data = dp.scan()
     label = data[:,2]
     input = data[:,:2]
-    model = models.model(input, label, max_epoch=300)
+    model, history = models.model(input, label, max_epoch=300)
+    models.savemodel(model, history)
+    print('----------------------training model is done----------------------')
 
     return model
 
-
-if __name__=='__main__':
-    model = trainer()
-    x = model.predict([[0.63, 2.3]])
-    y = np.round(x[0][0])
-    print(x)
-    print(y)
