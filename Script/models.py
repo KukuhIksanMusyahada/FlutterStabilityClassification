dd179cba0da91d229b9b8b931eacc44428ae1f63
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-07-09 09:48:50 +0700

    model utilities functions (save, load, predict)

--- Script/models.py
+++ Script/models.py
@@ -18,3 +18,45 @@ def model(X, y, max_epoch):
     model.fit(X,y, epochs= max_epoch, verbose=0)
 
     return model
+
+def SaveModel(model, history, optional_path: str=None):
+    """Save both model and history"""
+    now = datetime.datetime.now()
+    time_now = now.strftime('%Y%m%d%H%M%S')
+    folder_name = "ANN " + time_now
+    if optional_path != None:
+        model_directory = os.path.join (optional_path, folder_name)
+    else:
+        model_directory = os.path.join (ph.get_models_data(), folder_name)
+    os.makedirs(model_directory)
+    history_file = os.path.join(model_directory, 'history.pkl')
+
+
+    model.save(model_directory)
+    print ("\nModel saved to {}".format(model_directory))
+
+    with open(history_file, 'wb') as f:
+        pickle.dump(history, f)
+    print ("Model history saved to {}".format(history_file))
+
+
+def LoadModel(path_to_model):
+    """Load Model and optionally it's history as well"""
+    history_file = os.path.join(path_to_model, 'history.pkl')
+    model = tf.keras.models.load_model(path_to_model)
+    # model = tf.saved_model.load(path_to_model)
+    print ("\nmodel loaded")
+
+    with open(history_file, 'rb') as f:
+        history = pickle.load(f)
+    print ("model history loaded")
+
+    return model, history
+
+
+def predict(model, mach= None, vf=None):
+    input = [[mach, vf]]
+    pred = model.predict(input)
+    pred = np.round(pred[0][0])
+
+    return pred
